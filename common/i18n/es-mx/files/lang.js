195f4a0b6f41686c683e03a034fc76f45c481ca6
MaShiH <1457805270@QQ.COM>
2022-08-15 17:00:02 +0800

    葡萄牙转西班牙翻译完毕

--- common/i18n/es-mx/files/lang.js
+++ common/i18n/es-mx/files/lang.js
@@ -1,204 +1,200 @@
 export default {
 	index: {
-		yq:"Invitación",
-		cz: "Recargar",
+		yq:"Invitar",//Invitación
+		cz: "Recargar",//Recargar
 		cjwt:'FAQ',
-		gywm: 'Sobre nós',
-		czcg: "Recarga éxito",
-		txcg: 'Salida exitosa',
+		gywm: 'Sobre nosotros',// Sobre nós
+		czcg: "Recargar con éxito",// Recarga éxito
+		txcg: 'Retiro exitoso',// 提现成功
 	},
 	invite:{
-		yqpy:"Invitar amigos",
-		wdjj: 'Mi bono',
-		jjxq:'Detalles de bonificación',
-		zjyh: 'Usuario de inserción directa',
-		tdyh: 'Usuario del equipo',
-		rs: 'número de personas',
-		jj:"Bono",
-		yqhb:"Cartel de invitación",
-		mjj: 'Mi bono',
-		jjxq:'Detalles de bonificación',
-		jjjl:'Récord de bonificación',
-		jllx:'tipo de registro',
-		dinero: 'cantidad',
-		fuente: 'fuente',
-		time:'tiempo',
-		dinheiro: 'cantidad',
-		fonte: 'fuente',
-		hora:'hora',
-		fzcg:'éxito',
-		money:"Dinero",
-		source:"Fuente"
+		yqpy: 'Invitar a amigos',// "邀请朋友",Convidar amigos
+		wdjj: 'mi bono',// '我的奖金',Meu bônus
+		jjxq: 'Detalles de bonificación',// '奖金详情',Detalhes dos bônus
+		zjyh: 'Usuario de inserción directa',// '直推用户',Usuário de push direto
+		tdyh: 'Usuario del equipo',// '团队用户',Usuário da equipe
+		rs: 'Cantidad de personas',// '人数',número de pessoas
+		jj: 'Bonus',// "奖金",Bónus
+		yqhb: 'Cartel de invitación',// "邀请海报",Pôster Convite
+		mjj: 'Mi bono',// '我的奖金',Meu bônus
+		jjxq: 'Detalles de bonificación',// '奖金详情',Detalhes do bônus
+		jjjl: 'Registro de bonificación',// '奖金记录',Registro bônus
+		jllx: 'Tipo de registro',// '记录类型',tipo de registro
+		money: 'Monto',// '金额',Montante
+		source: 'Fuente',// '来源',Fonte
+		time: 'Tiempo',// '时间',Tempo
+		dinheiro: 'Cantidad',// '数量','quantia',
+		fonte: 'Origen',// '起源','fonte',
+		hora: 'Hora',// '小时','hora',
+		fzcg: 'éxito',// '成功','sucesso',
 	},
 	recharge:{
-		cz: "recargar",
-		jl:"grabar",
-		ye: "equilibrio",
-		cztd:'Canal de recarga',
-		tjdd: "Enviar pedido",
-		pkcancel:"cancelar",
-		pkconfirm:"confirmación",
+		cz:'Recargar',// "充值",recarregar
+		jl:'Registro',// "记录",registro
+		ye:'Balance',// "余额",saldo
+		cztd:'Canal de recarga',// '充值通道',Canal de recarga
+		tjdd:'Orden de envio',// "提交订单"Enviar pedido
+		pkcancel: 'Cancelar',// 取消"cancelar",
+		pkconfirm: 'Confirmar',// 确认"confirmação",
 	},
 	record:{
-		czjl:'recargar registro',
-		txjl: "Registro de retiro",
-		ddh:'número de pedido',
-		cjsj: 'crear tiempo',
-		czje:'cantidad de recarga',
-		dzje: 'Cantidad recibida',
-		ddzt:'Estado del pedido',
-		wcl: 'sin tratamiento',
-		ywc: 'Terminado',
-		yqx: 'Cancelado',
-		txje: 'monto de retiro',
-		sxf:'tarifa de manejo',
-		yjj: 'rechazado',
-		wsh: 'No revisado',
+		czjl:'Registro de recarga',// '充值记录',recarregar registro
+		txjl:'registro de retiros',// "提现记录",Registro de retirada
+		ddh:'Número de orden',// '订单号',número do pedido
+		cjsj:'Tiempo de creación',// '创建时间',criar tempo
+		czje:'Monto de recarga',// '充值金额',quantidade de recarga
+		dzje:'Cantidad recibida',// '到账金额',Valor recebido
+		ddzt:'Estado del pedido',// '订单状态',Status do pedido
+		wcl:'No procesado',// '未处理',sem tratamento
+		ywc:'Terminado',// '已完成',Concluído
+		yqx:'Cancelado',// '已取消',Cancelado
+		txje:'Cantidad de retiro',// '提现金额',quantidade de retirada
+		sxf:'Tasa de tramitación',// '手续费',taxa de manuseio
+		yjj:'Rechazado',// '已拒绝',rejeitado
+		wsh:'No revisado',// '未审核'Não revisado
 	},
 	sobre:{
-		gywm: 'Acerca de nosotros',
-		zxxq: 'Detalles de información'
+		gywm:'tungkol sa atin',// '关于我们',Sobre nós
+		zxxq:'Mga detalye ng impormasyon',// '资讯详情'Detalhes da informação
 	},
 	fazer:{
-		hqyj: "obtener comisión",
-		unfinished: 'No está terminado',
-		fulfill: 'Terminado',
-		bonus: 'Premios',
-		pay:'Pago',
-		zq: 'ganar dinero',
-		jrjl:'Grabar hoy',
-		jryj: 'comisión hoy',
-		djje: 'Congela la cantidad',
-		jrrw: 'Tarea de hoy',
-		ye:'Balance',
-		hqyj: 'Obtener comisión',
-		inacabado: 'deshecho',
-		cumprir: 'Resumir',
-		cply: 'Este producto proviene de: AliExpress',
-		bônus: 'prima',
-		pagar:'pagar',
-		title:'tesoro',
-		text:'Comienza a trabajar para descubrir tu tesoro de riqueza',
-		button:'participar',
+		zq:'Ganar dinero',// '赚钱',ganhar dinheiro
+		jrjl:'Récord de hoy',// '今日记录',Recorde de hoje
+		jryj:'Comisión hoy',// '今日佣金',Comissão de hoje
+		djje:'Cantidad congelada',// '冻结金额',Congele a quantidade
+		jrrw:'La tarea de hoy',// '今日任务',Tarefa de hoje
+		ye:'Balance',// '余额',Saldo
+		hqyj:'Obtener comisión',// '获取佣金',Obter comissão
+		unfinished:'Deshecho',// '未完成',Não está terminado
+		fulfill:'Terminado',// '已完成',Concluído
+		cply:'Este producto proviene de: AliExpress',// '此产品来自:速卖通',Este produto vem de: AliExpress
+		bonus:'Prima',// '奖金',Prémios
+		pay:'Pagar',// '支付',Pagamento
+		inacabado: 'Deshecho',//未完成,'inacabado',
+		cumprir: 'Resumir',//总结,'concluído',
+		cply: 'Este producto proviene de: AliExpress',//本产品来自：速卖通'Este produto vem de: AliExpress',
+		bônus: 'Prima',//奖金'bônus',
+		pagar: 'Pagar',//支付'pagar',
+		title:'Tesoro',//宝藏'tesouro',
+		text:'Comienza a trabajar para descubrir tu tesoro de riqueza',//开始工作以发现您的财富宝藏'Comienza a trabajar para descubrir tu tesoro de riqueza',
+		button:'Participar',// 加入'Junte-se',
 	},
 	tarefa:{
-		rwlb: 'lista de tareas',
-		ye:'equilibrio',
-		yj:"Comisión",
-		rw:'tarea',
-		dfbj: 'a devolver el principal',
-		dfyj:"Comisión a devolver",
-		jxz: 'progreso',
-		ywc: 'Terminado',
-		dwc: 'por completar',
-		djz: 'Congelación',
-		ddh:'número de pedido',
-		cjsj: 'crear tiempo',
-		yjbl: 'tasa de comisión',
-		ljyj:"Comisión acumulada",
-		cjje:"monto de la transacción",
-		rwjd:'Progreso del pedido',
-		ddzt:'Estado del pedido',
-		xq:'detalles',
-		ddxq:'Detalles del pedido',
-		fhyj: 'comisión de devolución',
-		fhbj: 'Devolver principal',
-		sm:'Descripción',
-		smxq: 'Después de completar el pago de los bienes, se actualizará el estado de la tarea.',
-		smt: 'Este producto proviene de: AliExpress',
-		yj:'comisión',
-		zf: 'pagar',
-		lxkf: 'El pedido se congeló, comuníquese con el servicio al cliente',
+		rwlb:'Lista de tareas', // '任务列表',lista de tarefas
+		ye:'Balance', // '余额',equilíbrio
+		yj:'Comisión', // "佣金",Comissão
+		rw:'Tarea', // '任务',tarefa
+		dfbj:'Principal a devolver', // '待返本金',para ser devolvido o principal
+		dfyj:'Comisión a devolver', // "待返佣金",Comissão a ser devolvida
+		jxz:'Procesando', // '进行中',andamento
+		ywc:'Terminado', // '已完成',Concluído
+		dwc:'A completar', // '待完成',a ser concluído
+		djz:'Congelación', // '冻结中',Congelando
+		ddh:'Número de orden', // '订单号',número do pedido
+		cjsj:'Tiempo de creación', // '创建时间',criar tempo
+		yjbl:'Porcentaje de comision', // '佣金比例',taxa de comissão
+		ljyj:'Comisión acumulada', // "累计佣金",Comissão acumulada
+		cjje:'Rotación', // "成交金额",valor da transação
+		rwjd:'Progreso del pedido', // '订单进度',Progresso do pedido
+		ddzt:'Estado del pedido', // '订单状态',Status do pedido
+		xq:'Detalles', // '详情',detalhes
+		ddxq:'Detalles del pedido', // '订单详情',Detalhes do pedido
+		fhyj:'Comisión de devolución', // '返回佣金',comissão de retorno
+		fhbj:'Devolver el principal', // '返还本金',Devolver o principal
+		sm:'Ilustrar', // '说明',Descrição
+		smxq:'El estado de la tarea se actualizará después de que se complete el pago de los bienes.', // '完成货物付款后，任务状态会更新.',Após concluir o pagamento das mercadorias, o status da tarefa será atualizado.
+		smt:'Este producto es de: AliExpress', // '此产品来自:速卖通',Este produto vem de: AliExpress
+		yj:'Comisión', // '佣金',comissão
+		zf:'Pagar', // '支付',pagar
+		lxkf:'El pedido se ha congelado, póngase en contacto con el servicio de atención al cliente', // '订单已冻结请联系客服',O pedido foi congelado, entre em contato com o atendimento ao cliente
 	},
 	minha:{
-		yy: "Idioma",
-		ye:'equilibrio',
-		jj: 'Bono',
-		cz:'recargar',
-		tx:'Retiro',
-		aqzx:'Centro de Seguridad',
-		td: 'equipo',
-		gywm: 'Acerca de nosotros',
-		kfzx:"Centro de Atención al Cliente",
-		aa:"idioma",
-		tc:"salir",
-		qdlkm: '¿Estás seguro de que quieres irte?',
-		qx:'Cancelar',
-		qd: 'OK',
-		dlp:'contraseña de inicio de sesión',
-		zfp:'contraseña de pago',
-		xmmk: 'La nueva contraseña no puede estar vacía',
-		mmbt: 'La entrada de contraseña secundaria es diferente',
-		pid:'nombre:',
+		ye:'Balance', //'余额',equilíbrio
+		jj:'Prima', //'奖金',Bônus
+		cz:'Recargar', //'充值',recarregar
+		tx:'Retirar', //'提现',Retirada
+		aqzx:'Centro de Seguridad', //'安全中心',Centro de Segurança
+		td:'Equipo', //'团队',equipe
+		gywm:'Sobre nosotros', //'关于我们',Sobre nós
+		kfzx:'Servicio al Cliente', //"客服中心",Centro de Atendimento ao Cliente
+		yy:'Idioma', //"语言",Linguagem
+		tc:'Abandonar', //"退出",sair
+		qdlkm:'Estás seguro que quieres irte', //'你确定要离开吗',Tem certeza que quer sair
+		qx:'Cancelar', //'取消',Cancelar
+		qd:'Por supuesto', //'确定',OK
+		dlp:'Contraseña de inicio de sesión', //'登录密码',senha de login
+		zfp:'Contraseña de pago', //'支付密码',senha de pagamento
+		xmmk:'La nueva contraseña no puede estar vacía', //'新密码不能为空',A nova senha não pode estar vazia
+		mmbt:'La segunda entrada de contraseña es diferente', //'二次密码输入不同',A entrada de senha secundária é diferente
+		pid:'nombre:', //'名字:',nome:
 	},
 	retirar:{
-		tx:'Retiro',
-		jl:'grabar',
-		ye:'equilibrio',
-		zxtxje: 'Cantidad mínima de retiro',
-		jcsxf:'Tarifa base',
-		sxfbl:'tasa de tasa',
-		txcg: 'Retiro exitoso',
-		txtip1: "¡El monto del retiro no puede ser inferior a 0!",
-		txtip2: '¡El monto de retiro no puede ser inferior al monto mínimo de retiro! ',
-		qrzh:'Dos cuentas diferentes',
-		zxtdje:'La cantidad máxima',
-		zxtime:'Tiempo de retiro',
-		pkcancel:"cancelar",
-		pkconfirm:"confirmación"
+		tx:'Retirar',//'提现',Retirada
+		jl:'Registro',//'记录',gravar
+		ye:'Balance',//'余额',equilíbrio
+		zxtxje:'Cantidad mínima de retiro',//'最小笔提现金额',Valor mínimo de retirada
+		jcsxf:'Tarifa básica de manejo',//'基础手续费',Taxa básica
+		sxfbl:'Relación de tarifas',//'手续费比例',taxa de taxa
+		txcg:'Retiro exitoso',//'提现成功',Retirada bem sucedida
+		txtip1:'El monto del retiro no puede ser inferior a 0!',//"提现金额不能小于0！",O valor do saque não pode ser menor que 0!
+		txtip2:'El monto del retiro no puede ser inferior al monto mínimo de retiro!',//'提现金额不能小于最小提现金额！'O valor do saque não pode ser menor que o valor mínimo do saque!
+		qrzh: 'Dos cuentas diferentes',//两个不同的账户'Duas contas diferentes',
+		zxtdje: 'Cantidad máxima',//最大金额'O montante máximo',
+		zxtime: 'Hora de salida',//退出时间'Tempo de retirada',
+		pkcancel:'Cancelar',// 取消"cancelar",
+		pkconfirm:'Confirmar',// 确认"confirmação"
 	},
 	safety:{
-		aqzx:'Centro de Seguridad',
-		xgmm:'Cambiar contraseña',
-		dlmm:'contraseña de inicio de sesión',
-		zfmm:'contraseña de pago',
-		jmm:"contraseña antigua",
-		xml:'nueva contraseña',
-		qrmm:'Confirmar contraseña',
-		qsrjmm:"Ingrese la contraseña anterior",
-		qsrxmm:"Por favor ingrese una nueva contraseña",
+		aqzx:'Centro de Seguridad', //'安全中心',Centro de Segurança
+		xgmm:'Cambiar la contraseña', //'修改密码',Alterar senha
+		dlmm:'Contraseña de inicio de sesión', //'登录密码',senha de login
+		zfmm:'Contraseña de pago', //'支付密码',senha de pagamento
+		jmm:'Contraseña anterior', //"旧密码",senha antiga
+		xmm:'Nueva contraseña', //'新密码',nova senha
+		qrmm:'Confirmar contraseña', //'确认密码',Confirmar senha
+		qsrjmm:'Por favor ingrese la contraseña anterior', //"请输入旧密码",Por favor, digite a senha antiga
+		qsrxmm:'Por favor ingrese una nueva contraseña', //"请输入新密码"Por favor, digite uma nova senha
 	},
 	service:{
-		kfzx: 'Centro de Atención al Cliente',
-		lxkf: 'Contactar servicio al cliente',
-		yy: 'idioma',
+		kfzx:'Servicio al Cliente',//'客服中心',Centro de Atendimento ao Cliente
+		lxkf:'Contactar Servicio al Cliente',//'联系客服',Entre em contato com o atendimento ao cliente
+		yy:'Idioma',//'语言'idioma
 	},
 	tarbar:{
-		sy:"página inicio",
-		hd: "actividad",
-		zq: 'Compras',
-		rw:"tarea",
-		wd:'mi',
-		kf: 'servicio al cliente',
-		cz: 'recargar',
+		sy:'Hogar', //"首页",pagina inicial
+		hd:'Actividad', //"活动",atividade
+		zq:'Ganar ESP', //'赚钱',Compras
+		rw:'Tarea', //"任务",tarefa
+		wd:'Mía', //'我的',meu
+		kf:'Servicio al Cliente', //'客服',atendimento ao cliente
+		cz:'Recargar', //'充值'recarregar
 	},
 	login:{
-		qsrsjh:"Ingrese el número de teléfono",
-		qsrmm: 'Por favor, introduzca la contraseña',
-		dl:'autenticación',
-		zczh:"registrar cuenta",
+		qsrsjh:'Por favor ingrese el número de teléfono', //"请输入手机号",Por favor, digite o número do telefone
+		qsrmm:'Por favor, ingrese contraseña', //'请输入密码',Por favor, digite a senha
+		dl:'Iniciar sesión', //'登录',autenticação
+		zczh:'Registrar una cuenta', //"注册账号"registrar conta
 	},
 	registry:{
-		qsrsjh: 'Ingrese el número de teléfono',
-		qsrmm:"Por favor ingrese la contraseña",
-		qzcsrmm: 'Por favor ingrese la contraseña nuevamente',
-		qsrzm: 'Ingrese el código de verificación',
-		qsryqm: 'Ingrese el código de invitación',
-		zc:'registrar',
-		name: 'Por favor ingrese un nombre',
-		qsrzqdyzm:"¡Ingrese el código de verificación correcto!",
-		mobileError:'Ingrese el número de teléfono móvil de 10-11 dígitos',
-		passwordError:'Dos entradas de contraseña inconsistentes',
+		qsrsjh:'Por favor ingrese el número de teléfono', //'请输入手机号',Por favor, digite o número de telefone
+		qsrmm:'Por favor, ingrese contraseña', //"请输入密码",Por favor, digite a senha
+		qzcsrmm:'Por favor ingrese la contraseña nuevamente', //'请再次输入密码',Por favor, digite a senha novamente
+		qsrzm:'Por favor ingrese el código de verificación', //'请输入验证码',Por favor, digite o código de verificação
+		qsryqm:'Por favor, introduzca el código de invitación', //'请输入邀请码',Por favor, digite o código de convite
+		zc:'Registrarse', //'注册',registrar
+		name:'Por favor ingrese un nombre', //'请输入名字',Por favor, digite um nome
+		qsrzqdyzm:'Ingrese el código de verificación correcto', //"请输入正确的验证码！",Por favor, digite o código de verificação correto!
+		mobileError:'Ingrese un número de teléfono móvil de 10-11 dígitos', //'请输入10-11位手机号',Digite o número de telefone celular de 10-11 dígitos
+		passwordError:'Contraseña inconsistente ingresada dos veces', //'两次密码输入不一致',Duas entradas de senha inconsistentes
 	},
 	newadd: {
-		xzapp: "Transferir aplicacao",
-		lssr: 'Renda histórica',
-		cxsr: 'Renda de promoção',
-		zzc: 'Ativos totais',
-		copy: 'copia',
-		chongzhi: 'Recharge',
-		tixian: 'Retirada',
-		yqm:"Código do convite:"
+		xzapp: 'Solicitud de transferencia', // "Transferir aplicacao",转让申请
+		lssr: 'Ingresos históricos', // 'Renda histórica',历史收入
+		cxsr: 'Ingresos promocionales', // 'Renda de promoção',促销收入
+		zzc: 'Los activos totales', // 'Ativos totais',总资产
+		copy: 'Copiar', // 'copia',复制
+		chongzhi: 'Recargar', // 'Recharge',充值
+		tixian: 'abandonar', // 'Retirada',退出
+		yqm:'Código de invitación:', // "Código do convite:"邀请码：
 	}
 }
-
