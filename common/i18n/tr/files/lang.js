c5b8c6bd651849ce743ebfed468af88b903bbe42
MaShiH <1457805270@QQ.COM>
2022-08-16 16:37:26 +0800

    新增土耳其语言包

---
+++ common/i18n/tr/files/lang.js
@@ -0,0 +1,201 @@
+export default { // 土耳其
+	index: {
+		yq:'Davet et', // "Davet et",Convidar
+		cz:'yükle', //"yükle",recarregar
+		cjwt:'çok sorulan sorular', // 'çok sorulan sorular',FAQ
+		gywm:'Hakkımızda', // Hakkımızda,Sobre nós
+		czcg:'yüklendi', // "yüklendi",recarga com sucesso
+		txcg:'Nakit çekildi', // 'Nakit çekildi',Retirada bem sucedida
+	},
+	invite:{
+		yqpy: 'Arkadaşları davet et',// "Arkadaşları davet et",Convidar amigos
+		wdjj: 'Ödülüm',// 'Ödülüm',Meu bônus
+		jjxq: 'Ödül ayrıntıları',// 'Ödül ayrıntıları',Detalhes dos bônus
+		zjyh: 'direk kullanıcı',// 'direk kullanıcı',Usuário de push direto
+		tdyh: 'Ekip kullanıcısı',// 'Ekip kullanıcısı',Usuário da equipe
+		rs: 'Kişi sayısı',// 'Kişi sayısı',número de pessoas
+		jj: 'Ödül',// "Ödül",Bónus
+		yqhb: 'davetiye afişi',// " davetiye afişi ",Pôster Convite
+		mjj: 'ödülümüz',// 'ödülümüz',Meu bônus
+		jjxq: 'Ödül ayrıntıları',// 'Ödül ayrıntıları',Detalhes do bônus
+		jjjl: 'Ödül kayıtları',// 'Ödül kayıtları',Registro bônus
+		jllx: 'kayıt türü',// ' kayıt türü ',tipo de registro
+		money: 'Tutar',// 'Tutar',Montante
+		source: 'kaynak',// 'kaynak',Fonte
+		time: 'Zaman',// Zaman',Tempo
+		dinheiro: 'Sayısı',// 'Sayısı','quantia',
+		fonte: 'Menşei',// ' Menşei ','fonte',
+		hora: 'saat',// 'saat','hora',
+		fzcg: 'başarılı',// 'başarılı','sucesso',
+	},
+	recharge:{
+		cz:'yükle',// "yükle",recarregar
+		jl:'kayıt',// "kayıt",registro
+		ye:'Tutar',// "Tutar",saldo
+		cztd:'Yükleme kanalı',// 'Yükleme kanalı',Canal de recarga
+		tjdd:'Siparişi Gönder',// " Siparişi Gönder "Enviar pedido
+		pkcancel: 'iptal et',// iptal et"cancelar",
+		pkconfirm: 'onayla',// onayla"confirmação",
+	},
+	record:{
+		czjl:'Yükleme kayıtları',// 'Yükleme kayıtları',recarregar registro
+		txjl:'Nakıt çekme kayıtları',// "Nakıt çekme kayıtları",Registro de retirada
+		ddh:'sipariş numaras',// sipariş numaras ',número do pedido
+		cjsj:'Zaman oluştur',// 'Zaman oluştur',criar tempo
+		czje:'yükleme tutarı',// 'yükleme tutarı',quantidade de recarga
+		dzje:'Hesaba gelen miktar',// 'Hesaba gelen miktar',Valor recebido
+		ddzt:'Sipariş durumu',// 'Sipariş durumu',Status do pedido
+		wcl:'işlenmemiş',// ' işlenmemiş,sem tratamento
+		ywc:'Tamamland',// 'Tamamlandı',Concluído
+		yqx:'İptal edildi',// 'İptal edildi',Cancelado
+		txje:'Nakıt çekim tutarı',// 'Nakıt çekim tutarı',quantidade de retirada
+		sxf:'işlem ücreti',// ' işlem ücreti ',taxa de manuseio
+		yjj:'reddedildi',// ' reddedildi ',rejeitado
+		wsh:'İncelenmemiş',// ' İncelenmemiş 'Não revisado
+	},
+	sobre:{
+		gywm:'Hakkımızda',// Hakkımızda,Sobre nós
+		zxxq:'Bilgi detayları',// ' Bilgi detayları 'Detalhes da informação
+	},
+	fazer:{
+		zq:'para kazan',// ' para kazan ',ganhar dinheiro
+		jrjl:'Bugünün rekoru',// ' Bugünün rekoru ',Recorde de hoje
+		jryj:'Bugünün komisyonu',// ' Bugünün komisyonu ',Comissão de hoje
+		djje:'Dondurulmuş tutar',// Dondurulmuş tutar',Congele a quantidade
+		jrrw:'Bugünün görevi',// ' Bugünün görevi ',Tarefa de hoje
+		ye:'Tutar',// 'Tutar',Saldo
+		hqyj:'Komisyonu al',// Komisyonu al ',Obter comissão
+		unfinished:'Tamamlanmadı',// 'Tamamlanmadı',Não está terminado
+		fulfill:'Tamamlandı',// 'Tamamlandı',Concluído
+		cply:'Bu ürün : AliExpress  adresten geliyor',// ' Bu ürün : AliExpress  adresten geliyor ',Este produto vem de: AliExpress
+		bonus:'Ödül',// 'Ödül',Prémios
+		pay:'Öde',// 'Öde',Pagamento
+		inacabado: 'Tamamlanmadı',//Tamamlanmadı,'inacabado',
+		cumprir: 'Özet',//Özet,'concluído',
+		cply: 'Bu ürün : AliExpress  adresten geliyor',// Bu ürün : AliExpress  adresten geliyor 'Este produto vem de: AliExpress',
+		bônus: 'Ödül',//Ödül'bônus',
+		pagar: 'Öde',//Öde'pagar',
+		title:'hazine',// hazine 'tesouro',
+		text:'Zenginlik hazinenizi keşfetmek için çalışmaya başlayın',// Zenginlik hazinenizi keşfetmek için çalışmaya başlayın'Comienza a trabajar para descubrir tu tesoro de riqueza',
+		button:'katıl',// katıl'Junte-se',
+	},
+	tarefa:{
+		rwlb:'görev listesi', // 'görev listesi',lista de tarefas
+		ye:'bakiye', // 'bakiye',equilíbrio
+		yj:'komisyon', // "komisyon",Comissão
+		rw:'görev', // 'görev',tarefa
+		dfbj:'İade edilecek tutar', // ' İade edilecek tutar',para ser devolvido o principal
+		dfyj:'İade edilecek komisyon', // " İade edilecek komisyon",Comissão a ser devolvida
+		jxz:'işleniyor', // ' işleniyor',andamento
+		ywc:'tamamlandı', // 'tamamlandı',Concluído
+		dwc:'tamamlanacak', // ' tamamlanacak',a ser concluído
+		djz:'donduruluyor', // 'donduruluyor',Congelando
+		ddh:'sipariş numarası', // 'sipariş numarası',número do pedido
+		cjsj:'oluşturma zamanı', // 'oluşturma zamanı',criar tempo
+		yjbl:'komisyon oranı', // 'komisyon oranı',taxa de comissão
+		ljyj:'Birikmiş komisyon', // " Birikmiş komisyon",Comissão acumulada
+		cjje:'İşlem Tutarı', // " İşlem Tutarı",valor da transação
+		rwjd:'Sipariş ilerlemesi', // ' Sipariş ilerlemesi',Progresso do pedido
+		ddzt:'Sipariş durumu', // ' Sipariş durumu',Status do pedido
+		xq:'detaylar', // 'detaylar',detalhes
+		ddxq:'Sipariş ayrıntıları', // ' Sipariş ayrıntıları',Detalhes do pedido
+		fhyj:'komisyon iade et', // 'komisyon iade et',comissão de retorno
+		fhbj:'Anaparayı iade et', // ' Anaparayı iade et ',Devolver o principal
+		sm:'Kullanma Klavuz', // 'Kullanma Klavuz',Descrição
+		smxq:'Malların ödemesi tamamlandığında görevin durumu güncellenecektir.', // ' Malların ödemesi tamamlandığında görevin durumu güncellenecektir.',Após concluir o pagamento das mercadorias, o status da tarefa será atualizado.
+		smt:'Bu ürün  AliExpress  adresten geliyor', // ' Bu ürün  AliExpress  adresten geliyor ',Este produto vem de: AliExpress
+		yj:'komisyon', // 'komisyon',comissão
+		zf:'öde', // 'öde',pagar
+		lxkf:'Sipariş donduruldu, lütfen müşteri hizmetleri ile iletişime geçin', // ' Sipariş donduruldu, lütfen müşteri hizmetleri ile iletişime geçin ',O pedido foi congelado, entre em contato com o atendimento ao cliente
+	},
+	minha:{
+		ye:'bakiye', //'bakiye',equilíbrio
+		jj:'ödül', //'ödül',Bônus
+		cz:'yükle', //'yükle',recarregar
+		tx:'nakit para çek', //'nakit para çek',Retirada
+		aqzx:'güvenlik merkezi', //'güvenlik merkezi',Centro de Segurança
+		td:'ekip', //'ekip',equipe
+		gywm:'Hakkımızda', //' Hakkımızda ',Sobre nós
+		kfzx:'Müşteri Hizmetleri Merkezi', //" Müşteri Hizmetleri Merkezi ",Centro de Atendimento ao Cliente
+		yy:'Dil', //"Dil",Linguagem
+		tc:'geri çık', //"geri çık",sair
+		qdlkm:'Gitmek istediğinizden emin misiniz', //' Gitmek istediğinizden emin misiniz ',Tem certeza que quer sair
+		qx:'iptal et', //'iptal et',Cancelar
+		qd:'onayla', //'onayla',OK
+		dlp:'girişşifresi', //'girişşifresi',senha de login
+		zfp:'ödeme şifresi', //'ödeme şifresi',senha de pagamento
+		xmmk:'yeni şifre boş olamaz', //'yeni şifre boş olamaz',A nova senha não pode estar vazia
+		mmbt:'girilen şifre eşit değil', //' girilen şifre eşit değil ',A entrada de senha secundária é diferente
+		pid:'adı:', //'adı:',nome:
+	},
+	retirar:{
+		tx:'nakit çek',//'nakit çek',Retirada
+		jl:'kayıt',//'kayıt',gravar
+		ye:'bakiye',//'bakiye',equilíbrio
+		zxtxje:'Minimum para çekme tutarı',//' Minimum para çekme tutarı',Valor mínimo de retirada
+		jcsxf:'Temel hizmet ücreti',//' Temel hizmet ücreti',Taxa básica
+		sxfbl:'işlem ücret oranı',//'işlem ücret oranı',taxa de taxa
+		txcg:'Nakit para çekildi',//'Nakit para çekildi',Retirada bem sucedida
+		txtip1:'Nakıt çekme miktarı 0'dan az olamaz!',//" Nakıt çekme miktarı 0'dan az olamaz！",O valor do saque não pode ser menor que 0!
+		txtip2:'Çekme miktarı minimum para çekme miktarından az olamaz!',//' Çekme miktarı minimum para çekme miktarından az olamaz！'O valor do saque não pode ser menor que o valor mínimo do saque!
+		qrzh: 'iki farklı hesap',// iki farklı hesap'Duas contas diferentes',
+		zxtdje: 'maksimum tutar',//maksimum tutar'O montante máximo',
+		zxtime: 'Çıkış zamanı',// Çıkış zamanı'Tempo de retirada',
+		pkcancel:'iptal et',// iptal et"cancelar",
+		pkconfirm:'onayla',// onayla"confirmação"
+	},
+	safety:{
+		aqzx:'güvenlik merkezi', //'güvenlik merkezi',Centro de Segurança
+		xgmm:'şifreyi değiştir', //'şifreyi değiştir',Alterar senha
+		dlmm:'giriş şifresi', //'giriş şifresi',senha de login
+		zfmm:'ödeme şifresi', //'ödeme şifresi',senha de pagamento
+		jmm:'eski şifre', //"eski şifre",senha antiga
+		xmm:'yeni şifre', //'yeni şifre',nova senha
+		qrmm:'şifreyi onayla', //'şifreyi onayla',Confirmar senha
+		qsrjmm:'lütfen eski şifreyi girin', //"lütfen eski şifreyi girin",Por favor, digite a senha antiga
+		qsrxmm:'lütfen yeni şifreyi girin', //" lütfen yeni şifreyi girin "Por favor, digite uma nova senha
+	},
+	service:{
+		kfzx:'Müşteri Merkezi',//' Müşteri Merkezi' ',Centro de Atendimento ao Cliente
+		lxkf:'Müşteri Hizmetleri ileiletişime geçin',//' Müşteri Hizmetleri ileiletişime geçin',Entre em contato com o atendimento ao cliente
+		yy:'dil',//'dil'idioma
+	},
+	tarbar:{
+		sy:'Ana sayfa', //"Ana sayfa",pagina inicial
+		hd:'faaliyet', //"faaliyet",atividade
+		zq:'para kazan', //'para kazan',Compras
+		rw:'görev', //"görev",tarefa
+		wd:'benim', //benim',meu
+		kf:'Müşteri Hizmetleri', //' Müşteri Hizmetleri ',atendimento ao cliente
+		cz:'yükle', //'yükle'recarregar
+	},
+	login:{
+		qsrsjh:'Lütfen telefon numaranızı girin', //" Lütfen telefon numaranızı girin",Por favor, digite o número do telefone
+		qsrmm:'Lütfen şifreyi girin', //' Lütfen şifreyi girin',Por favor, digite a senha
+		dl:'Giriş yap', //' Giriş yap',autenticação
+		zczh:'kayıtlı hesap', //" kayıtlı hesap"registrar conta
+	},
+	registry:{
+		qsrsjh:'Lütfen telefon numaranızı girin', //' Lütfen telefon numaranızı girin',Por favor, digite o número de telefone
+		qsrmm:'Lütfen şifrenizi giriniz', //" Lütfen şifrenizi giriniz",Por favor, digite a senha
+		qzcsrmm:'Lütfen şifreyi tekrar girin', //' Lütfen şifreyi tekrar girin',Por favor, digite a senha novamente
+		qsrzm:'Lütfen doğrulama kodunu girin', //' Lütfen doğrulama kodunu girin',Por favor, digite o código de verificação
+		qsryqm:'Davet kodunu giriniz', //' Davet kodunu giriniz',Por favor, digite o código de convite
+		zc:'Kayıt', //' Kayıt',registrar
+		name:'Lütfen adınızı girin', //' Lütfen adınızı girin',Por favor, digite um nome
+		qsrzqdyzm:'Lütfen doğru doğrulama kodunu girin', //" Lütfen doğru doğrulama kodunu girin!",Por favor, digite o código de verificação correto!
+		mobileError:'Lütfen 10-11 haneli cep telefonu numarasını girin', //' 'Lütfen 10-11 haneli cep telefonu numarasını girin',Digite o número de telefone celular de 10-11 dígitos
+		passwordError:'İki şifre girişi tutarsız', //' 'İki şifre girişi tutarsız ',Duas entradas de senha inconsistentes
+	},
+	newadd: {
+		xzapp: 'devir başvurusu', // "Transferir aplicacao",devir başvurusu
+		lssr: 'Renda histórica', // 'Renda histórica', geçmiş geliri
+		cxsr: 'promosyon geliri', // 'Renda de promoção', promosyon geliri
+		zzc: 'toplam varlıklar', // 'Ativos totais', toplam varlıklar
+		copy: 'kopyala', // 'copia',kopyala
+		chongzhi: 'yükle', // 'Recharge',yükle
+		tixian: 'çıkış', // 'Retirada', çıkış
+		yqm:'Davet kodu:', // "Código do convite:"Davet kodu：
+	}
+}
+
